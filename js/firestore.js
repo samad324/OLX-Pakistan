10a9d8f5f1e4030d494a6c68b2764f6b7409b1a8
samad324 <a.s324889@gmail.com>
2018-07-05 00:30:14 +0500

    OLX Pakistan

---
+++ js/firestore.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyB-D2oL6e261Ejfa9nlzKNodgIXkrkTVPc",
+    authDomain: "whatsapp324889.firebaseapp.com",
+    databaseURL: "https://whatsapp324889.firebaseio.com",
+    projectId: "whatsapp324889",
+    storageBucket: "whatsapp324889.appspot.com",
+    messagingSenderId: "441553373571"
+  };
+  firebase.initializeApp(config);
+
+
+  var firestore = firebase.firestore();
---
+++ src/login/js/firebase.js
@@ -0,0 +1,32 @@
+var config = {
+    apiKey: "AIzaSyD5ALJ3Pvfn86ybcI6-uWUuKn8xBL7Fweo",
+    authDomain: "olxpakistanpwa.firebaseapp.com",
+    databaseURL: "https://olxpakistanpwa.firebaseio.com",
+    projectId: "olxpakistanpwa",
+    storageBucket: "olxpakistanpwa.appspot.com",
+    messagingSenderId: "573103718935"
+};
+firebase.initializeApp(config);
+
+
+const firestore = firebase.firestore();
+
+
+function LoginUser(event){
+    event.preventDefault();
+    
+    let email = document.getElementById("email").value;
+    let passsord = document.getElementById("password").value;
+
+    let btn = document.getElementById("btn");
+    btn.innerHTML = `
+        <img src = "images/loading.gif" style=width : 14px>
+    `
+    firebase.auth().signInWithEmailAndPassword(email,passsord)
+        .then(function(res){
+            console.log(res)
+            btn.innerHTML = "login"
+        }).catch(function(err){
+            console.log(err.message)
+        })
+}
